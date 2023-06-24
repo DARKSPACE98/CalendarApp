497825bde924a855d6c861b3adf041c325dbfbfc
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-06-24 00:57:46 -0700

    Se agrega store y uiSlices para el calendarApp

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { uiSlice } from "./";
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export const store = configureStore({
+
+    reducer: {
+        ui: uiSlice.reducer
+    }
+
+});
