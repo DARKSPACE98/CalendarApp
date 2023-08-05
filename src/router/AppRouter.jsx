295e607e761de67be67c177ba31c51e0eb1d90a7
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-08-05 01:11:43 -0700

    Mantener el estado de la autenticacion

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,35 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth"
 import { CalendarPage } from "../calendar"
+import { useAuthStore } from "../hooks"
+import { useEffect } from "react"
 
 
 export const AppRouter = () => {
+
+    const { status, checkAuthToken } = useAuthStore();
+    // const authStatus = 'not-authenticated';
+
+    useEffect(() => {
+      checkAuthToken();
+    }, [])
     
-    const authStatus = 'not-authenticated';
-    
-    
+
+    if ( status === 'checking' ) {
+        return (
+            <h3>Cargando...</h3>
+        )
+    }
+
     return (
-    <Routes>
-        
-        {
+        <Routes>
+            {
+                    ( status === 'not-authenticated')
+                    ? <Route path="/auth/*" element={ <LoginPage />} />
+                    : <Route path="/*" element={ <CalendarPage />} />
+            }
             
-            (authStatus === 'not-authenticated')
-             ?<Route path="/auth/*" element={<LoginPage />} />
-             :<Route path="/*" element={<CalendarPage />} />
-        }    
-        
-        <Route path="/*" element={<Navigate to="/auth/login" />} />
-        
-    </Routes>
-  )
+            <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+        </Routes>
+    )
 }
